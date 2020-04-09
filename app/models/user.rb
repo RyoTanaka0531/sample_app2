5c53708000256944e740e2356986d8ef923327d8
EC2 Default User <ch5cxcxxh@gmail.com>
2020-04-09 14:57:38 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
+    attr_accessor :remember_token
     before_save {self.email = email.downcase }
     before_save {email.downcase!}
     validates :name, presence: true, length: {maximum: 50}
@@ -25,6 +26,10 @@ class User < ApplicationRecord
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
     end
-
+    
+    def authenticated?(remember_token)
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
 end
+
 #
