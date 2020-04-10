3a3da48aa6ba9d930546038ddb6a99c3be05a66d
EC2 Default User <ch5cxcxxh@gmail.com>
2020-04-10 03:00:45 +0000

    upddate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
-    attr_accessor :remember_token
     before_save {self.email = email.downcase }
     before_save {email.downcase!}
     validates :name, presence: true, length: {maximum: 50}
