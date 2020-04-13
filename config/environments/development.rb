9934828f87da908752ae46b83c4acdf0ed922006
EC2 Default User <ch5cxcxxh@gmail.com>
2020-04-13 09:47:06 +0000

    Update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.web_console.development_only = false
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-  config.web_console.whitelisted_ips = '60.121.254.181'
+  
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
