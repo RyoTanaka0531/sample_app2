4c294fe8264c8aa4853b1705000b48213a244cff
EC2 Default User <ch5cxcxxh@gmail.com>
2020-04-14 03:05:58 +0000

    update micropost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,10 +47,12 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+  config.web_console.development_only = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.web_console.development_only = false
+  
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
