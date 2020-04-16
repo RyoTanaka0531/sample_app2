035c5ce49f896d10327d67079b1cfd9015a1ab9e
EC2 Default User <ch5cxcxxh@gmail.com>
2020-04-16 07:11:39 +0000

    update follow

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,15 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,  only: [:create, :destroy]
   root "static_pages#home"
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
